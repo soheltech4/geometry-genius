f1a476dae51bb238d928cf0c772699298d05a18b
sohelranagtc <sohelranagtc@gmail.com>
2023-02-18 00:52:31 +0600

    Get Value Function and set id

--- Java Script/common.js
+++ Java Script/common.js
@@ -1,3 +1,19 @@
+// Function To get input Value
+function GetValue(inputId){
+    const InputValueElement = document.getElementById(inputId)
+    const InputValueString = InputValueElement.value;
+    const InputValue = parseInt(InputValueString);
+    InputValueElement.value = '';
+    return InputValue;
+}
+
+// 
+
+
+
 document.getElementById('triangle-btn').addEventListener('click', function(){
-    console
+    const TriangleValueB = GetValue('triangle-b')
+    const TriangleValueH = GetValue('triangle-h')
+    console.log(TriangleValueB, TriangleValueH)
+    
 })
